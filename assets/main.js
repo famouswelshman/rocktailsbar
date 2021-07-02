eabf20344e438b862a0bc96ac910fc2e658ab206
James Hunt <famouswelshman@yahoo.co.uk>
2021-07-02 11:40:34 +0100

    Amended the size of google maps api on index. Added a new div to host the links to other pages.

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 10,
+        center: { lat: 38.980496, lng: 1.305204 },
+        zoom: 15,
     });
 }
