963a3b4685b950530f4c77d0355f648d7d5f212a
James Hunt <famouswelshman@yahoo.co.uk>
2021-07-09 11:47:49 +0100

    Added a javascript code for index page to show the opening times upon button click.

--- assets/main.js
+++ assets/main.js
@@ -16,3 +16,13 @@ function toggleTheme(theme) {
         body.classList.add('light')
     }
 }
+/*----------------- This is the button function script for Opening times on the index page-----*/
+function myFunction() {
+    document.getElementById("opening-times2").innerHTML = "Mon - 4pm until 4am";
+    document.getElementById("opening-times3").innerHTML = "Tue - 4am until 4pm";
+    document.getElementById("opening-times4").innerHTML = "Wed - 4am until 4pm";
+    document.getElementById("opening-times5").innerHTML = "Thur - 4am until 4pm";
+    document.getElementById("opening-times6").innerHTML = "Fri - 4am until 4pm";
+    document.getElementById("opening-times7").innerHTML = "Sat - 4am until 4pm";
+    document.getElementById("opening-times8").innerHTML = "Sun - Closed";
+}
