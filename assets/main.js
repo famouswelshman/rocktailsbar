22aa8838cf387845e1cfc65180c0cdb5840bc318
James Hunt <famouswelshman@yahoo.co.uk>
2021-07-25 19:15:01 +0100

    Updated the contact form fields code.

--- assets/main.js
+++ assets/main.js
@@ -27,3 +27,26 @@ function initializeOpeningTimes() {
         x.style.display = "block";
     }
 }
+
+/** ----- Function calls on  */
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        btn.value = 'Sending...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_bve9m98';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+    });
